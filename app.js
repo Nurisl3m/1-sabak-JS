6b376dd9b309da42e742094ed427ddc1c7ee4dda
Nurisl3m <nurislam20122309@gmail.com>
2024-08-26 11:39:31 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const userName=prompt('How old are you')
+console.log(`hello`);
+const isMerried=confirm('You are a programmer')
+// alert=('userName')
+
+const num=parseInt(prompt("first san"))
+const num2= parseInt(prompt("sekend san"))
+
+
+const we=num*num2
+console.log(we);
